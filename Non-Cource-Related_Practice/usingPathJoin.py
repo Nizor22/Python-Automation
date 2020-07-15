91a1754a9e0f23921edb86a97ce9e1f06ace7826
Nizor22 <meganizor22@gmail.com>
2020-07-15 10:51:41 -0400

    Modified the readme file and got rid of the non cource related folder.

--- Non-Cource-Related_Practice/usingPathJoin.py
+++
@@ -1,7 +0,0 @@
-import os
-from pathlib import Path
-searchDir = Path('C:/Users/megan/Desktop')
-
-for entry in os.listdir(searchDir):
-    filePath = os.path.join(searchDir, entry)
-    fh = open(filePath)
